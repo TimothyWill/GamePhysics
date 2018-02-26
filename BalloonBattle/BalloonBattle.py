c3ae52cd14f081963d265ed97e4d0e73c858cceb
Tim Will <willt1646@my.uwstout.edu>
2018-02-25 20:04:56 -0600

    Added menu system

--- BalloonBattle/BalloonBattle.py
+++ BalloonBattle/BalloonBattle.py
@@ -17,15 +17,15 @@ GRAY     = ( 127, 127, 127)
 
 def main():
     pygame.init()
+    
+    #Import background
     background = pygame.image.load("background.jpg")
     background = pygame.transform.scale(background, (800, 600))
  
+    #Initialize screen and coords
     width = 800
     height = 600
     screen = pygame.display.set_mode([width,height])
-    draw_screen = screen.copy()
-    draw_screen.fill(WHITE)
-    draw_screen.set_alpha(64) # 64/255 is about 25% opaque
     screen_center = Vec2d(width/2, height/2)
     coords = Coords(screen_center.copy(), 1, True)
     # ^ Center of window is (0,0), scale is 1:1, and +y is up
@@ -33,13 +33,20 @@ def main():
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
 
+    # Initialize menu
+    startButton = pygame.Rect(250, 300, 300, 50)
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    title = myfont.render('BALLOON FORCE', False, (0, 0, 0))
+    play = myfont.render('Play', False, (0, 0, 0))
+
     # Initialize the balloons
-    LeftBalloon  = Balloon(Vec2d(0, 0), 30, Vec2d(-1.5, 0), 0.05, BLUE, 9.8, pygame.K_w)
-    RightBalloon = Balloon(Vec2d(0, 0), 30, Vec2d(1.5, 0), 0.05, BLUE, 9.8, pygame.K_i)
+    LeftBalloon  = Balloon(Vec2d(0, 0), 30, Vec2d(-1.5, 1), 0.05, BLUE, 9.8, pygame.K_w)
+    RightBalloon = Balloon(Vec2d(0, 0), 30, Vec2d(1.5, 1), 0.05, BLUE, 9.8, pygame.K_i)
     RightBalloon.image = pygame.transform.flip(RightBalloon.image, True, False)
 
+    #Cannon ball collector
     objects = []
-    #objects.append(Circle(Vec2d(-1.5,0), Vec2d(1, 1), 1, 0.05, BLUE))
+
     coords.zoom_at_coords(Vec2d(0,0), 200) 
     # ^ Zoom in 200x, so now the scale is 100 pixels per unit
     # -------- Main Program Loop -----------\
@@ -60,49 +67,80 @@ def main():
             # Apply Gravity
             obj.force += Vec2d(0, -9.8);
         
-    
-    done = False
-    while not done:
-        # --- Main event loop
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: # If user clicked close
-                done = True
-            if event.type == pygame.KEYDOWN:
-                keys = pygame.key.get_pressed()
-                if keys[pygame.K_e]: #Press Space to shoot left cannon
-                    cannon(Vec2d(LeftBalloon.pos.x + (65/200), LeftBalloon.pos.y - (140/200)), Vec2d(3, 3))
-                if keys[pygame.K_u]:
-                    cannon(Vec2d(RightBalloon.pos.x + (60/200), RightBalloon.pos.y - (140/200)), Vec2d(-3, 3))
-                
-                
-        # Move each object according to physics
-        for obj in objects:
-            obj.update(dt)
-            
-            # Remove objects off the screen
-            if (obj.pos.y < -2):
-                objects.remove(obj)
-                del obj
-            
-        # Update the balloons
-        LeftBalloon.update(dt)
-        RightBalloon.update(dt)
+    # Main game loop
+    exitGame = False
+    while not exitGame:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exitGame = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos  # gets mouse position
 
-        # Drawing
-        screen.fill(WHITE) # wipe the screen
-        screen.blit(draw_screen, (0, 0)) # draw the trail semitransparent
+                # checks if mouse position is over the button
+                if startButton.collidepoint(mouse_pos):
+                    done = False
+                    while not done:
+                        # --- Main event loop
+                        for event in pygame.event.get(): 
+                            if event.type == pygame.QUIT: # If user clicked close
+                                done = True
+                                exitGame = True
+                            if event.type == pygame.KEYDOWN:
+                                keys = pygame.key.get_pressed()
+                                if keys[pygame.K_e]: #Press Space to shoot left cannon
+                                    cannon(Vec2d(LeftBalloon.pos.x + (65/200), LeftBalloon.pos.y - (140/200)), Vec2d(3, 3))
+                                if keys[pygame.K_u]:
+                                    cannon(Vec2d(RightBalloon.pos.x + (60/200), RightBalloon.pos.y - (140/200)), Vec2d(-3, 3))
+                                
+                                
+                        # Move each object according to physics
+                        for obj in objects:
+                            obj.update(dt)
+                            
+                            # Remove objects off the screen
+                            if (obj.pos.y < -2):
+                                objects.remove(obj)
+                                del obj
+                            
+                        # Update the balloons
+                        LeftBalloon.update(dt)
+                        RightBalloon.update(dt)
+                    
+                        # Draw background
+                        screen.blit(background, (0,0))
+                        
+                        # Draw cannon balls
+                        for obj in objects:
+                            obj.draw(screen, coords) # draw object to screen
+                            #obj.draw(draw_screen, coords) # add object to trail in draw_screen
+                            
+                        # Draw the balloons
+                        LeftBalloon.draw(screen, coords)
+                        RightBalloon.draw(screen, coords)
+                        
+                        # --- Update the screen with what we've drawn.
+                        pygame.display.update()
+                    
+                        # This limits the loop to 60 frames per second
+                        clock.tick(frame_rate)
+              
+        # Draw background
         screen.blit(background, (0,0))
-        for obj in objects:
-            obj.draw(screen, coords) # draw object to screen
-            #obj.draw(draw_screen, coords) # add object to trail in draw_screen
             
+        # Draw buttons
+        pygame.draw.rect(screen, GRAY, startButton)
+    
+        # Draw Text
+        screen.blit(title, (275,100))
+        screen.blit(play, (380,300))
+        
         # Draw the balloons
         LeftBalloon.draw(screen, coords)
         RightBalloon.draw(screen, coords)
         
         # --- Update the screen with what we've drawn.
         pygame.display.update()
-    
+                    
         # This limits the loop to 60 frames per second
         clock.tick(frame_rate)
         
