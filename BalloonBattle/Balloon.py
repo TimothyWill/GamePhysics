a9d21354ee95af207a401eff0cd9590887c9bcea
Nicholas Reynolds <reynoldsn6924@my.uwstout.edu>
2018-02-26 13:03:23 -0600

    Updated Balloon Physics

--- BalloonBattle/Balloon.py
+++ BalloonBattle/Balloon.py
@@ -3,52 +3,82 @@ from vec2d import Vec2d
 
 class Balloon:
     
-    def __init__(self, pVel, pMass, pPos, pRadius, pColor, pHeat, pUpKey):
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, pVel, pMass, pPos, pRadius, pColor, pAir, pUpKey):
         self.image = pygame.image.load("hotairballoon.png")
         self.image = pygame.transform.scale(self.image, (130, 170))
-        self.pos = Vec2d(pPos.x - (65/200), pPos.y - (85/200))
+        self.pos = pPos
         self.vel = pVel
         self.mass = pMass
         self.mom = pMass * pVel
         self.color = pColor
-        self.heat = pHeat
-        self.heatLoss = 3;
+        self.air = pAir
+        self.airLoss = 3
         self.upKey = pUpKey
         self.radius = pRadius
+        self.outsideTemp = 292
+        self.insideTemp = 394
+        self.heatLossConstant = 1
+        self.densityConstant = 1
+        self.insideDensity = 1
+        self.outsideDensity = 1
+        self.lampTemp = 100
+
         
     def update(self, dt):
-        #print("HEAT: " + str(self.heat))
-        # lose heat
-        self.heat -= self.heatLoss * dt
-        # add heat
+        # lose air
+        self.air -= self.airLoss * dt
+        
+        
+        # lose temp to heat
+        self.loseHeat(dt)
+        
+        # add air
         keys = pygame.key.get_pressed()
         if keys[self.upKey]:
-           # print("Button Pressed")
-            self.heat += dt * 6;
+            print("Button Pressed")
+            self.air += dt * 6;
+            self.addHeat(dt)
         else:
-            self.heat -= dt;
+            self.air -= dt
+    
+        # calculate density
+        self.calculateDensity()
 
-        # Check heat amount
-        if self.heat <= 7:
-            self.heat = 7  
-        elif self.heat >= 13:
-            self.heat = 13
+        # Check air amount
+        if self.air <= 5:
+            self.air = 5  
+        elif self.air >= 13:
+            self.air = 13
         
-        # Calculate force from the heat
-        self.force = Vec2d(0, self.heat)
+        self.calculateBuoyantForce()
+        print(self.force)
         
         # Apply Gravity
         self.force += Vec2d(0, -9.8)
-        
-        # Apply Air Resistance
-        self.force -= (100 * self.vel.get_length() ** 2) * self.vel.normalized();
         
         self.mom += self.force*dt
         self.vel.copy_in(self.mom/self.mass)
         self.pos += self.vel*dt
         
+    def loseHeat(self, dt):
+        self.insideTemp -= -self.heatLossConstant * (self.outsideTemp - self.insideTemp) * dt
+        print ("Temperature: " + str(self.insideTemp))
+
+    def addHeat(self, dt):
+        self.insideTemp += self.lampTemp * dt
         
-        
+    def calculateDensity(self):
+        self.insideDensity = self.outsideTemp * self.outsideDensity / self.insideTemp
+        print("Density: " + str(self.insideDensity))
+
+    def calculateBuoyantForce(self):
+        self.force = Vec2d(0, (self.outsideDensity - self.insideDensity) * 100)
+    
     def draw(self, screen, coords):
-        screen.blit(self.image,coords.pos_to_screen(self.pos).int())
+        pygame.draw.circle(screen, self.color, 
+                           coords.pos_to_screen(self.pos).int(), 
+                           int(coords.scalar_to_screen(self.radius)), 0)
+        
+        
+        
+        
--- BalloonBattle/BalloonBattle.py
+++ BalloonBattle/BalloonBattle.py
@@ -4,7 +4,7 @@ import pygame
 from vec2d import Vec2d
 from coords import Coords
 from Cannon import Cannon
-from Balloon import Balloon
+from Balloon2 import Balloon
 
 # Define some colors
 BLACK    = (   0,   0,   0)
@@ -42,8 +42,8 @@ def main():
     quitGame = myfont.render('Quit', False, (0, 0, 0))
 
     # Initialize the balloons
-    LeftBalloon  = Balloon(Vec2d(0, 0), 30, Vec2d(-1.5, 1), 0.05, BLUE, 9.8, pygame.K_w)
-    RightBalloon = Balloon(Vec2d(0, 0), 30, Vec2d(1.5, 1), 0.05, BLUE, 9.8, pygame.K_i)
+    LeftBalloon  = Balloon(Vec2d(0, 0), 80, Vec2d(-1.5, 1), 0.05, BLUE, 9.8, pygame.K_w)
+    RightBalloon = Balloon(Vec2d(0, 0), 80, Vec2d(1.5, 1), 0.05, BLUE, 9.8, pygame.K_i)
     RightBalloon.image = pygame.transform.flip(RightBalloon.image, True, False)
 
     #Cannon balls
