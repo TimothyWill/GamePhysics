341d7e34b2b03ca7fb305aef08015ac0026a8f88
Nicholas Reynolds <reynoldsn6924@my.uwstout.edu>
2018-03-09 15:12:47 -0600

    Old file version

--- Gravity/gravity_simulation_stub.py
+++ Gravity/gravity_simulation_stub.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 27 13:56:44 2017
-
 @author: sinkovitsd
 """
 import pygame
@@ -57,7 +56,7 @@ def gravity_force(obj1, obj2):
     r = obj1.pos - obj2.pos # distance between two objects
     m1 = obj1.mass # mass of 1st object
     m2 = obj2.mass # mass of 2nd object
-    G = 6.67384*(10**(-11)) # gravitational constant
+    G = 1 #6.67384*(10**(-11)) # gravitational constant
     if r.mag() > (obj1.radius + obj2.radius):
         force = -((G*m1*m2)/r.mag2())*r.hat() # this is the formula for gravity
     else:
@@ -66,18 +65,17 @@ def gravity_force(obj1, obj2):
     obj1.force += force
     obj2.force -= force
 
-def addNewBall(width, height, objects, zoom, position, coords, velocity):
+def addNewBall(width, height, objects, zoom, position, coords):
     print("Adding a new Ball")
     radius = massSlider.getValue()
+    radius = uniform(0.3, 0.8)
     mass = radius*radius*20
-    objects.append(Circle(coords.pos_to_coords(position), velocity,
+    objects.append(Circle(coords.pos_to_coords(position), 2*Vec2d(uniform(-1,1), uniform(-1,1)),
                               mass, radius, random_bright_color()))
     print("Position: " + str(objects[len(objects) - 1].pos))
     return
 
 def main():
-    mouseDownCoords = Vec2d(-1, -1)
-    
     pygame.init()
  
     width = 800
@@ -85,7 +83,7 @@ def main():
     screen = pygame.display.set_mode([width,height])
     screen_center = Vec2d(width/2, height/2)
     coords = Coords(screen_center.copy(), 1, True)
-    zoom = 0.01
+    zoom = 80
     coords.zoom_at_coords(Vec2d(0,0), zoom) 
     
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
@@ -97,10 +95,10 @@ def main():
     # Create initial objects to demonstrate
     objects = []
     n = 20
-    mass = 6*(10**24)
-    radius = 4000
+    mass = 1
+    radius = 0.2
     for i in range(n):
-        radius = uniform(3000, 6000)
+        radius = uniform(0.3, 0.8)
         mass = radius*radius*20
         objects.append(Circle(Vec2d(width/zoom*uniform(-0.5,0.5), 
                                     height/zoom*uniform(-0.5,0.5)),
@@ -110,7 +108,7 @@ def main():
 
     # -------- Main Program Loop -----------\
     frame_rate = 60
-    playback_speed = 60000 # 1 is real time, 10 is 10x real speed, etc.
+    playback_speed = 1 # 1 is real time, 10 is 10x real speed, etc.
     dt = playback_speed/frame_rate
     print("timestep =", dt)
     
@@ -118,15 +116,6 @@ def main():
     done = False
     while not done:
         # --- Main event loop
-        
-        # Add a new ball
-        if pygame.mouse.get_pressed()[0]:
-            print("Button Pressed")
-            mouseDownCoords = Vec2d(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-        elif mouseDownCoords.x != -1:
-            print("Button Released")
-            addNewBall(width, height, objects, zoom, pygame.mouse.get_pos(), coords, Vec2d(0, 0))
-            mouseDownCoords = Vec2d(-1, -1)
         
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: # If user clicked close
@@ -137,7 +126,9 @@ def main():
                     pause = not pause
                 if keys[pygame.K_0]:
                     center(objects)
-
+            # Add a new ball
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                addNewBall(width, height, objects, zoom, pygame.mouse.get_pos(), coords)
 
 
         if pause:
@@ -150,7 +141,7 @@ def main():
                 
             screen.fill(GRAY, pygame.Rect(300, 200, 200, 50), pygame.BLEND_ADD)
             screen.blit(pauseText, (350, 200))
-        else:
+        else:            
             # Physics
             # Calculate the force on each object
             for obj in objects:
