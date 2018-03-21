556b8de712dfbd823ac13c6e817095096c34d94c
Jeremiah6464 <castleknightart@gmail.com>
2018-03-21 15:25:04 -0500

    Fixed Shit

--- Gravity/CollidingCircles.py
+++ Gravity/CollidingCircles.py
@@ -39,13 +39,31 @@ def gravity_force(obj1, obj2):
     m1 = obj1.mass # mass of 1st object
     m2 = obj2.mass # mass of 2nd object
     G = 1 #6.67384*(10**(-11)) # gravitational constant
-    if r.mag() > (obj1.radius + obj2.radius):
-        force = -((G*m1*m2)/r.mag2())*r.hat() # this is the formula for gravity
-    else:
-        force = 1*(r.mag()-obj1.radius)*r.hat() # this is the formula for repulsion
+    force = -((G*m1*m2)/r.mag2())*r.hat() # this is the formula for gravity
+    #force = 1*(r.mag()-obj1.radius)*r.hat() # repulsion force
     
     obj1.force += force
     obj2.force -= force
+
+def collides(obj1, obj2):
+    if (obj1.pos.get_distance(obj2.pos) < obj1.radius + obj2.radius):
+        
+        m1 = obj1.mass
+        m2 = obj2.mass
+        r = obj1.pos - obj2.pos
+        u = 1/((1/m1) + (1/m2))
+        v1 = obj1.vel
+        v2 = obj2.vel
+        n = (obj2.pos - obj1.pos).normalized()
+        J = 1.6*u*((v2 - v1).dot(n))
+        d = obj1.radius + obj2.radius - (obj1.pos - obj2.pos).mag()
+        #L = u*r.cross(v1 - v2)
+        #J2 = -d*(L)/r*(r+d)
+        obj1.pos = obj1.pos - (u/m1)*d*n
+        obj2.pos = obj2.pos + (u/m2)*d*n
+        if (J < 0):
+            obj1.mom += J * n
+            obj2.mom -= J * n
 
 def main():
     pygame.init()
@@ -66,10 +84,10 @@ def main():
     radius = 1
     mass = radius*radius*20
     objects.append(Circle(Vec2d(-1, 0), Vec2d(0,0), mass, radius, random_bright_color()))
-    objects.append(Circle(Vec2d(2, 0), Vec2d(0, 0), mass, radius, random_bright_color()))
-    objects.append(Circle(Vec2d(2, 4), Vec2d(0, 0), mass, radius, random_bright_color()))
-    objects.append(Circle(Vec2d(0, -2), Vec2d(0, 0), mass, radius, random_bright_color()))
-    objects.append(Circle(Vec2d(6, 1), Vec2d(-10, 0), mass, radius, random_bright_color()))
+    #objects.append(Circle(Vec2d(2, 0), Vec2d(0, 0), mass, radius, random_bright_color()))
+    #objects.append(Circle(Vec2d(2, 4), Vec2d(0, 0), mass, radius, random_bright_color()))
+    #objects.append(Circle(Vec2d(0, -2), Vec2d(0, 0), mass, radius, random_bright_color()))
+    objects.append(Circle(Vec2d(6, 1), Vec2d(-1, 0), mass, radius, random_bright_color()))
 
 
     # -------- Main Program Loop -----------\
@@ -84,31 +102,23 @@ def main():
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: # If user clicked close
                 done = True
-    
+                
+        for obj in objects:
+            obj.force.zero()
+        for i1, obj1 in enumerate(objects):
+                for i2, obj2 in enumerate(objects):
+                    if i1 < i2:
+                        gravity_force(obj1, obj2)
+                        
             
         # Move each object according to physics
         for obj in objects:
             obj.update(dt)
             
-            
         for i1, obj1 in enumerate(objects):
                 for i2, obj2 in enumerate(objects):
                     if i1 < i2:
-                        # Apply gravity force
-                        gravity_force(obj1, obj2)
-                        
-                        m1 = obj1.mass
-                        m2 = obj2.mass
-                        r = obj1.pos - obj2.pos
-                        u = 1/((1/m1) + (1/m2))
-                        v1 = obj1.vel
-                        v2 = obj2.vel
-                        nHat = r.hat()
-                        
-                        if r.mag() < (objects[0].radius + objects[1].radius) and (v2 - v1).dot(nHat) > 0:
-                            J = 1.6*u*((v2 - v1).dot(nHat)) * nHat
-                            obj1.mom += J
-                            obj2.mom -= J
+                        collides(obj1, obj2)
         
 
         # Drawing
