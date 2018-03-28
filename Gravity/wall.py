5645a324bf509ac4bdb5b539be819f08bd9db70a
TimothyWill <willt1646@my.uwstout.edu>
2018-03-28 14:45:24 -0500

    Finished Wall Class

--- Gravity/wall.py
+++ Gravity/wall.py
@@ -8,12 +8,14 @@ import pygame
 from vec2d import Vec2d
 
 class Wall:
-    def __init__(self, pos, vel, color):
-        self.pos = pos
+    def __init__(self, pos1, pos2, vel, color):
+        self.pos1 = pos1
+        self.pos2 = pos2
         self.vel = vel
         self.color = color
         self.mom = self.vel*self.mass
         self.force = Vec2d(0,0)  
+        self.normal = self.makeNormal(self.pos1, self.pos2)
     def update_vel(self):
         self.vel.copy_in(self.mom/self.mass)
     def update_pos(self, dt):
@@ -28,6 +30,7 @@ class Wall:
         pygame.draw.circle(screen, self.color, 
                            coords.pos_to_screen(self.pos).int(), 
                            int(coords.scalar_to_screen(2)), 0)
-    def normal:
-        
+   
+    def makeNormal(self, pos1, pos2):
+        return Vec2d(pos1.x-pos2.x, pos1.y-pos2.y).perpendicular().normalized()
  
