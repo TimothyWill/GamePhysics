99cddfa4878a175b68beb6ecdea11a6cd5cb8ada
Jeremiah6464 <castleknightart@gmail.com>
2018-04-06 15:35:31 -0500

    Added Friction

--- Gravity/CollidingCircles.py
+++ Gravity/CollidingCircles.py
@@ -74,6 +74,7 @@ def collideWithWalls(obj, wall):
     R = obj.radius
     n = wall.normal
     d = R - (n.dot(obj.pos - wall.pos1))
+    tangent = n.perpendicular_normal()
     if (d > 0):
         m = obj.mass
         #r = obj.pos - wall.pos1
@@ -88,10 +89,24 @@ def collideWithWalls(obj, wall):
         if (J < 0):
             obj.mom -= J * n
             
+        Fric = -obj.mass * obj.vel.dot(tangent)
+        cf = 0.75 # Coefficient of Friction
+        
+        if (abs(Fric) > cf * J):
+            ratio = cf * J/abs(Fric)
+            Fric *= ratio
+        else:
+            ratio = 1
+        
+        ratio *= obj.vel.dot(tangent)/obj.vel.dot(n)
+       # obj.pos -= (obj.radius - d) * ratio * tangent
+        
+        obj.mom -= Fric * tangent
+            
 # Add a new circle to the scene
 def addNewBall(width, height, objects, zoom, position, coords, velocity):
     # Get the radius of the ball from the slider
-    radius = randint(1,5)
+    radius = randint(1,3)
     # Calculate the mass based on the radius
     mass = radius
     # Add the circle to the array
@@ -120,11 +135,11 @@ def main():
     mass = 1
     radius = 1
     mass = radius*radius*20
-    objects.append(Circle(Vec2d(-5, 5), Vec2d(0,0), 2*mass, 2*radius, random_bright_color()))
-    objects.append(Circle(Vec2d(2, 0), Vec2d(0, 0), mass, radius, random_bright_color()))
-    objects.append(Circle(Vec2d(2, 4), Vec2d(0, 0), mass/3, radius/3, random_bright_color()))
-    objects.append(Circle(Vec2d(0, -2), Vec2d(0, 0), mass/2, radius/2, random_bright_color()))
-    objects.append(Circle(Vec2d(2, 0), Vec2d(-1, 0), 4*mass, 4*radius, random_bright_color()))
+    #objects.append(Circle(Vec2d(-5, 5), Vec2d(0,0), 2*mass, 2*radius, random_bright_color()))
+    #objects.append(Circle(Vec2d(2, 0), Vec2d(0, 0), mass, radius, random_bright_color()))
+    #objects.append(Circle(Vec2d(2, 4), Vec2d(0, 0), mass/3, radius/3, random_bright_color()))
+    #objects.append(Circle(Vec2d(0, -2), Vec2d(0, 0), mass/2, radius/2, random_bright_color()))
+    #objects.append(Circle(Vec2d(2, 0), Vec2d(-1, 0), 4*mass, 4*radius, random_bright_color()))
     
     walls.append(Wall(Vec2d(10,0), Vec2d(0,-10), 0, BLACK))
     walls.append(Wall(Vec2d(0,-10), Vec2d(-10,-2), 0, BLACK))
---
+++ Gravity/rotating circle.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 14:39:31 2018
+
+@author: Student
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 13:56:44 2017
+
+@author: sinkovitsd
+"""
+import pygame
+from vec2d import Vec2d
+
+class RotatingCircle(Circle):
+    def __init__(self, pos, vel, mass, radius, color, angle=0, angvel=0, linecolor):
+        super().__init__(pos, vel, mass, radius, color)
+        self.angle = angle
+        self.angvel = angvel
+        self.moment = 0.5*self.mass*self.radius*self.radius
+        self.angmom = self.moment*self.angvel
+        self.torque = 0
+        self.linecolor = linecolor
+    
+    def update_mom(self, dt):
+        self.mom += self.force*dt
+        self.angmom += self.torque*dt
+        self.update_vel()
+        self.update_angvel()
+        
+    def set_vel(self, vel):
+        self.vel.copy_in(vel)
+        self.mom.copy_in(self.vel*self.mass)
+
+    def update_vel(self):
+        self.vel.copy_in(self.mom/self.mass)
+    
+    def update_angvel(self):
+        self.angvel = self.angmom/self.moment
+
+    def update_pos(self, dt):
+        self.pos += self.vel*dt
+        self.angle += self.angvel*dt
+
+    def update(self, dt):
+        self.update_mom(dt)
+        self.update_pos(dt)
+        
+    def impulse(self, imp, point=None):
+        self.mom +=imp
+        self.update_vel()
+        if point is not None:
+            self.angmom += (point - self.pos).cross(imp)
+            self.update_angvel()
+                
+    def draw(self, screen, coords):
+        #print("radius", self.radius, coords.scalar_to_screen(self.radius))
+        super.draw(screen, coords)
+        vec = self.radius*Vec2d(cos(self.angle), sin(self.angle))
+        pygame.draw.circle(screen, self.linecolor, 
+                           coords.pos_to_screen(self.pos).int(), 
+                           coords.pos_to_screen(self.pos + vec).int())
+ 
